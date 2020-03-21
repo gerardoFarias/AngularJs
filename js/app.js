fd7ef12f688ca84be67ea2293bfbd7f7c0c5b6b6
gerardoFarias <61151545+gerardoFarias@users.noreply.github.com>
2020-03-21 00:21:35 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,36 @@
+var myApp = angular.module('myApp', []);
+
+
+
+myApp.controller('myController', function($scope){
+    
+
+	
+	$scope.newUser = {};
+	$scope.checkedUser = {};
+	$scope.message = "";
+
+	$scope.users = 
+	[
+        {FNAME:"Nikhil",LNAME:"Singh",EMAIL:"nikhilsingh2311@gmail.com"},
+        {FNAME:"xccx",LNAME:"Sinxcxcgh",EMAIL:"nikcvxcvcxvxchilsingh2311@gmail.com"}
+	];
+	$scope.saveUser = function(){
+		$scope.users.push($scope.newUser);
+		$scope.newUser = {};
+		$scope.message = "User added Successful";
+	};
+
+	$scope.selectUser = function(user){
+		console.log(user);
+		$scope.clickedUser = user;
+	};
+
+	$scope.updateUser = function(user){
+
+	};
+
+	$scope.deleteUser = function(){
+		$scope.users.splice($scope.users.indexOf($scope.clickedUser), 1);
+	};
+});
